ae4419232f67cd245fd590bea1f39ebf00b63d68
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-10 18:02:19 +0530

    folder_structure_created

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int bucky[][]={{12,2,3,4,3},{1,2,3},{1,2,3,4,5}} ;
+
+                System.out.println();
+
+
+    }
+}
---
+++ src/main/java/com/stackRoute/test.java
@@ -0,0 +1,4 @@
+package com.stackRoute;
+
+public class test {
+}
