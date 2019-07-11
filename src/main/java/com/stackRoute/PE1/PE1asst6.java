cddf1a443b9f79a7318f69720753d251a5d8fbc9
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-11 21:29:21 +0530

    PE1asst6 created with test cases

---
+++ src/main/java/com/stackRoute/PE1/PE1asst6.java
@@ -0,0 +1,17 @@
+package com.stackRoute.PE1;
+
+public class PE1asst6 {
+    public String givenCharTypeIs(char chr1){
+
+
+        if (chr1>=97 && chr1<=122)
+            return "Lower Case";
+        else if (chr1>=65 && chr1<=90)
+            return "Capital Letter";
+        else if(chr1>=48 && chr1<=57)
+            return "Number";
+        else
+            return "Special Character";
+
+    }
+}
---
+++ src/test/java/com/stackRoute/PE1/PE1asst6Test.java
@@ -0,0 +1,12 @@
+package com.stackRoute.PE1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PE1asst6Test {
+
+    @Test
+    public void givenCharTypeIs() {
+    }
+}
