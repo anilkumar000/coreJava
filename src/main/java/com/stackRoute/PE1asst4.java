aa63d15f3d7ed0d53405ab6936e2b64d32894523
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-11 12:53:53 +0530

    PE1asst4 completed

--- src/main/java/com/stackRoute/PE1asst4.java
+++ src/main/java/com/stackRoute/PE1asst4.java
@@ -1,4 +1,19 @@
 package com.stackRoute;
 
+import java.util.Scanner;
+
 public class PE1asst4 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int n=in.nextInt();
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+        }
+    }
 }
