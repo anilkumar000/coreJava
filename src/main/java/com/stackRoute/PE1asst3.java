f0e4b5c7a6512f57b63828dd1a886933fb84edab
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-11 12:30:49 +0530

    PE1asst3_completed

--- src/main/java/com/stackRoute/PE1asst3.java
+++ src/main/java/com/stackRoute/PE1asst3.java
@@ -2,38 +2,38 @@ package com.stackRoute;
 import java.util.Scanner;
 public class PE1asst3 {
     public static void main(String[] args) {
-        int n,i=0;
+
+
 
         Scanner in = new Scanner(System.in);
 
         System.out.print("Input an alphabet: ");
         String input = in.next().toLowerCase();
 
-        n = input.length();
 
-        boolean lowercase = input.charAt(i) >= 97 && input.charAt(i) <= 122;
-        boolean vowels =input.equals("a") || input.equals("e") || input.equals("i")
-                || input.equals("o") || input.equals("u");
+        for (int i=0;i<input.length();i++){
 
-        if (n > 1) {
+            char temp=input.charAt(i);
+            if (temp >= 'a' && temp <= 'z') {
 
-            for (i=0; i < n; i++) {
-                if (lowercase) {
+                switch(temp){
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        System.out.println("vowel");
+                        break;
+                        default:
+                            System.out.println("consonant");
 
-                    if (vowels)
-                        System.out.println("Vowel");
-                    else
-                        System.out.println("Consonant");
+              }
 
-                }
+
             }
-
-        }
-        else if (lowercase) {
-            if (vowels)
-                System.out.println("Input letter is Vowel");
             else
-                System.out.println("Input letter is Consonant");
+                System.out.println("It is not an alphabet");
         }
+
     }
 }
